d5d722edfaec4514bb0d34569ab19560f96d291e
thibault.poire <thibault.poire@103F26.etu.iut-lens.univ-artois.fr>
2012-11-26 10:21:16 +0100

    Création de la classe Alien

--- src/fr/dubois/space/invader/SpaceInvaderView.java
+++ src/fr/dubois/space/invader/SpaceInvaderView.java
@@ -99,6 +99,7 @@ public class SpaceInvaderView extends View {
 		super.onDraw(canvas);
 		canvas.drawRGB(0, 0, 0);
 		canvas.drawRect(0, 0, TARGET_WIDTH-1, TARGET_HEIGHT-1, paint);
+		
 		if (text != null){
 			canvas.drawText(text, canvas.getWidth()/2,canvas.getHeight()/2, paint);
 		}
